4bb3937b6be3e767059521ab6ba0ddd713f19087
LokeshMehar <meharlokesh395@gmail.com>
2025-01-26 23:03:12 +0530

    idk

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter } from 'react-router-dom';
 import Navbar from './components/Navbar';
+import Hero from './components/Hero';
 
 function App() {
   return (
@@ -7,7 +8,7 @@ function App() {
       <div className="relative z-0 bg-primary">
         <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
           <Navbar/>
-          
+          <Hero/>
         </div>
         <div className="relative z-0">
         </div>
