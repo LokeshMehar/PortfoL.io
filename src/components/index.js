623884ded1ea1fc5dfc816edc4d9eef79357fde5
LokeshMehar <meharlokesh395@gmail.com>
2025-01-26 23:55:31 +0530

    added anothe comp

--- src/components/index.js
+++ src/components/index.js
@@ -2,11 +2,13 @@ import { EarthCanvas, BallCanvas, ComputersCanvas, StarsCanvas } from './canvas'
 import Hero from './Hero';
 import Navbar from './Navbar';
 import About from './About';
+import Tech from './Tech';
 
 export {
   Hero,
   Navbar,
   About,
+  Tech,
   EarthCanvas, 
   BallCanvas, 
   ComputersCanvas, 
