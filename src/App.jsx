d2c03f9e8ed7d29ed07789bbe6d37ede8d91a670
LokeshMehar <meharlokesh395@gmail.com>
2025-01-26 11:47:53 +0530

    changed the layout.tsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
-
+import { BrowserRouter } from 'react-router-dom';
 
 function App() {
   return (
-    <>
-      <h1 className="text-4xl font-bold text-blue-500">PortfoL.io</h1>
-    </>
-  )
+    <BrowserRouter>
+      <div className="relative z-0 bg-primary">
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+        </div>
+        <div className="relative z-0">
+        </div>
+      </div>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
